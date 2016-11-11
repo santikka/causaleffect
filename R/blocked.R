811a0b9b94fa172a7c57a4d1132fd9cff132f5e2
santikka <santtu.tikka@jyu.fi>
2016-11-11 07:51:51 -0500

    reverted

--- R/blocked.R
+++ R/blocked.R
@@ -1,6 +1,6 @@
 blocked <- function(w, y, G.x.overbar, to) {
   nv <- vcount(G.x.overbar)
-  connected <- unique(unlist(neighborhood(G.x.overbar, order = nv, nodes = y, mode = "in")))
+  connected <- unique(unlist(neighborhood(G.x.overbar, order = nv, nodes = y, mode = "all")))
   r <- V(G.x.overbar)[setdiff(1:nv, connected)]$name
   r <- to[which(to %in% r)]
   return (r)

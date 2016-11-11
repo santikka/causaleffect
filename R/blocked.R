84ea58d178aff9e0289d540bf0d250a64dc6afbf
santikka <santtu.tikka@jyu.fi>
2016-11-11 00:04:29 -0500

    fixed direction of incoming edges

--- R/blocked.R
+++ R/blocked.R
@@ -1,6 +1,6 @@
 blocked <- function(w, y, G.x.overbar, to) {
   nv <- vcount(G.x.overbar)
-  connected <- unique(unlist(neighborhood(G.x.overbar, order = nv, nodes = y, mode = "all")))
+  connected <- unique(unlist(neighborhood(G.x.overbar, order = nv, nodes = y, mode = "in")))
   r <- V(G.x.overbar)[setdiff(1:nv, connected)]$name
   r <- to[which(to %in% r)]
   return (r)

25539953c31e2f26398f9a184095ec5bafccc280
santikka <santtu.tikka@jyu.fi>
2016-11-17 07:32:00 -0500

    fixed primes

--- R/get.expression.R
+++ R/get.expression.R
@@ -1,5 +1,5 @@
 get.expression <- function(x, primes = FALSE) {
-  query <- attr(x, "query")
-  prime.counter <- setNames(rep(1, length(query)), unlist(query))
+  query <- unlist(attr(x, "query"))
+  prime.counter <- setNames(rep(1, length(query)), query)
   return (get.expression.internal(x, primes, prime.counter, FALSE))
 }

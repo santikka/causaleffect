372943e8a20cc8649ec09cb1c20bd6273d7880b0
santikka <santtu.tikka@jyu.fi>
2016-11-24 20:05:22 -0500

    fixed issue with expressions

--- R/get.expression.R
+++ R/get.expression.R
@@ -5,6 +5,8 @@ get.expression <- function(x, primes = FALSE) {
   single.source <- FALSE
   if (!is.null(attr(x, "algorithm"))) {
     if (attr(x, "algorithm") == "zid") target.sym <- "("
+  }
+  if (!is.null(attr(x, "sources"))) {
     if (attr(x, "sources") == 1) single.source <- TRUE
   }
   return(get.expression.internal(x, primes, prime.counter, FALSE, target.sym, single.source))

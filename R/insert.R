ff4dc2479a4b9fa6d5de989e99e5626b6bb8d630
Santtu Tikka <santtu.tikka@jyu.fi>
2017-05-10 15:33:43 +0300

    finalize 1.3.4

--- R/insert.R
+++ R/insert.R
@@ -1,24 +1,33 @@
 insert <- function(J, D, M, cond, S, O, G.adj, G, G.obs, topo) {
-  mis <- M[which(M %in% D)]
-  M <- M[length(mis)]
+  mis.ind <- which(M %in% D)
+  if (length(mis.ind) == 0) return(list(J, D))
+  mis <- M[mis.ind]
+  M <- mis[length(mis)]
   if (M %in% cond) return(list(J, D))
-  non.mis <- min(which(J %in% O))
-  V.prev <- J[non.mis]
+  # non.mis <- min(which(J %in% O))
+  # V.prev <- J[non.mis]
+  # ind <- which(topo == V.prev)
+  # V.pi <- topo[0:(ind-1)]
+  # anc <- ancestors(M, G.obs, topo)
+  # cond.diff <- setdiff(V.pi, anc)
+  # ds <- powerset(cond.diff, nonempty = FALSE)
+  J.min <- min(which(J %in% topo))
+  V.prev <- J[J.min]
   ind <- which(topo == V.prev)
   V.pi <- topo[0:(ind-1)]
-  anc <- ancestors(M, G.obs, topo)
-  cond.diff <- setdiff(union(M, setdiff(V.pi, anc)), J)
-  ds <- powerset(cond.diff, nonempty = FALSE)
+  ds <- powerset(setdiff(V.pi, M), nonempty = FALSE)
   n <- length(ds)
   for (i in 1:n) {
-    add <- union(anc, ds[[i]])
-    add.M <- setdiff(add, M)
+    # add <- union(anc, ds[[i]])
+    # add.M <- setdiff(add, M)
+    # a.set <- union(setdiff(add, D), setdiff(D, add))
+    add <- union(ds[[i]], M)
     a.set <- union(setdiff(add, D), setdiff(D, add))
     if (wrap.dSep(G.adj, J, a.set, setdiff(D, a.set)) && 
-        wrap.dSep(G.adj, M, S, setdiff(add.M, S))) {
+        wrap.dSep(G.adj, M, S, setdiff(ds[[i]], S))) {
       J.new <- union(J, M)
-      D.new <- add.M
-      return(list(J.new, D.new, M, add.M))
+      D.new <- ds[[i]]
+      return(list(J.new, D.new, M, ds[[i]]))
     }
   }
   return(list(J, D))
--- R/join.R
+++ R/join.R
@@ -6,23 +6,30 @@ join <- function(J, D, vari, cond, S, M, O, G.adj, G, G.obs, topo) {
     D.new <- cond
     return(list(J.new, D.new))
   }
-  non.mis <- min(which(J %in% O))
-  V.prev <- J[non.mis]
+  # non.mis <- min(which(J %in% O))
+  # V.prev <- J[non.mis]
+  # ind <- which(topo == V.prev)
+  # anc <- ancestors(vari, G.obs, topo)
+  # cond.diff <- setdiff(V.pi, anc)
+  # ds <- powerset(cond.diff, nonempty = FALSE)
+  J.min <- min(which(J %in% topo))
+  V.prev <- J[J.min]
   ind <- which(topo == V.prev)
   V.pi <- topo[0:(ind-1)]
-  anc <- ancestors(vari, G.obs, topo)
-  cond.diff <- setdiff(union(vari, setdiff(V.pi, anc)), J)
-  ds <- powerset(cond.diff, nonempty = FALSE)
+  ds <- powerset(setdiff(V.pi, vari), nonempty = FALSE)
   n <- length(ds)
   for (i in 1:n) {
-    add <- union(anc, ds[[i]])
-    add.v <- setdiff(add, vari)
-    a.set <- union(setdiff(add, D), setdiff(D, add))
-    b.set <- union(setdiff(add.v, cond), setdiff(cond, add.v))
+    # add <- union(anc, ds[[i]])
+    # add.v <- setdiff(add, vari)
+    # a.set <- union(setdiff(add, D), setdiff(D, add))
+    # b.set <- union(setdiff(add.v, cond), setdiff(cond, add.v))
+    add <- union(ds[[i]], vari)
+    a.set <- union(setdiff(add, D), setdiff(D, add)) 
+    b.set <- union(setdiff(ds[[i]], cond), setdiff(cond, ds[[i]]))
     if (wrap.dSep(G.adj, J, a.set, setdiff(D, a.set)) && 
         wrap.dSep(G.adj, vari, b.set, setdiff(cond, b.set))) {
       J.new <- union(J, vari)
-      D.new <- add.v
+      D.new <- ds[[i]]
       return(list(J.new, D.new))
     }
   }

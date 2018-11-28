ed7569d4583522a26199a9b92ad6a39792ba991d
Santtu Tikka <santtu.tikka@jyu.fi>
2018-11-28 15:40:06 +0200

    fix placement of tr nodes in sotr

--- R/tr.target.R
+++ R/tr.target.R
@@ -1,12 +1,13 @@
 tr.target <- function(Z, W, G.obs, G, topo.obs) {
     # return(setdiff(descendants(Z, G.obs, topo.obs), W))
     target <- setdiff(descendants(Z, G.obs, topo.obs), W)
-    #G.xbar <- subgraph.edges(G, E(G)[!(to(Z) | (from(Z) & (description == "U" & !is.na(description))))], delete.vertices = FALSE) # remove id nonid
-    #nontarget <- setdiff(ancestors(W, G.xbar, topo.obs), target) # remove if nonid
+    G.xbar <- subgraph.edges(G, E(G)[!(to(Z) | (from(Z) & (description == "U" & !is.na(description))))], delete.vertices = FALSE) # remove id nonid
+    G.xbar.obs <- observed.graph(G.xbar)
+    nontarget <- setdiff(ancestors(W, G.xbar.obs, topo.obs), target)
     cc <- c.components(G, topo.obs)
     for (Wi in W) {
       target <- union(target, Find(function(x) Wi %in% x, cc))
     }
-    #target <- setdiff(target, nontarget) # remove if nonid
+    target <- setdiff(target, nontarget)
     return(target %ts% topo.obs)
 }

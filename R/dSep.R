ee8cf9f4265b5b53b23786714b5e995c4cf38329
santikka <santtuth@gmail.com>
2021-11-10 15:47:36 +0200

    fix for d-separation for larger graphs

--- R/dSep.R
+++ R/dSep.R
@@ -11,97 +11,60 @@
 dSep <- function(G, x, y, z) {
   an_z <- ancestors_unsrt(z, G)
   an_xyz <- ancestors_unsrt(union(union(x, y), z), G)
-  stack_top <- length(x)
-  stack_size <- max(stack_top, 64)
-  stack <- rep(FALSE, stack_size)
-  stack[1:stack_top] <- TRUE
-  names(stack)[1:stack_top] <- x
-  visited_top <- 0
-  visited_size <- 64
-  visited <- rep(FALSE, visited_size)
-  names(visited) <- rep(NA, visited_size)
-  is_visited <- FALSE
-  while (stack_top > 0) {
-    is_visited <- FALSE
-    el <- stack[stack_top]
-    el_name <- names(el)
-    stack_top <- stack_top - 1
-    if (visited_top > 0) {
-      for (i in 1:visited_top) {
-        if (el == visited[i] && identical(el_name, names(visited[i]))) {
-          is_visited <- TRUE
+  n <- length(igraph::V(G))
+  v <- igraph::V(G)$name
+  direction <- NA
+  traverse_up <- logical(n)
+  visited_up <- logical(n)
+  traverse_down <- logical(n)
+  visited_down <- logical(n)
+  names(traverse_up) <- v
+  names(visited_up) <- v
+  names(traverse_down) <- v
+  names(visited_down) <- v
+  traverse_up[x] <- TRUE
+  visit <- FALSE
+  while (any(traverse_up) || any(traverse_down)) {
+    visit <- FALSE
+    for (j in 1:n) {
+      if (traverse_up[j]) {
+        traverse_up[j] <- FALSE
+        if (!visited_up[j]) {
+          visit <- TRUE
+          direction <- TRUE
+          el_name <- v[j]
           break
         }
       }
-    }
-    if (!is_visited) {
-      if (el_name %in% y) return(FALSE)
-      visited_top <- visited_top + 1
-      if (visited_top > visited_size) {
-        visited_old <- visited
-        visited_size_old <- visited_size
-        visited_size <- visited_size * 2
-        visited <- rep(FALSE, visited_size)
-        visited[1:visited_size_old] <- visited_old
-        names(visited[1:visited_size_old]) <- names(visited_old)
+      if (traverse_down[j]) {
+        traverse_down[j] <- FALSE
+        if (!visited_down[j]) {
+          visit <- TRUE
+          direction <- FALSE
+          el_name <- v[j]
+        }
       }
-      visited[visited_top] <- el
-      names(visited)[visited_top] <- el_name
-      if (el && !(el_name %in% z)) {
+    }
+    if (visit) {
+      if (el_name %in% y) return(FALSE)
+      if (direction) {
+        visited_up[j] <- TRUE
+      } else {
+        visited_down[j] <- TRUE
+      }
+      if (direction && !(el_name %in% z)) {
         visitable_parents <- intersect(setdiff(parents_unsrt(el_name, G), el_name), an_xyz)
         visitable_children <- intersect(setdiff(children_unsrt(el_name, G), el_name), an_xyz)
-        n_vis_pa <- length(visitable_parents)
-        n_vis_ch <- length(visitable_children)
-        if (n_vis_pa + n_vis_ch > 0) {
-          while (n_vis_pa + n_vis_ch + stack_top > stack_size) {
-            stack_old <- stack
-            stack_size_old <- stack_size
-            stack_size <- stack_size * 2
-            stack <- rep(FALSE, stack_size)
-            stack[1:stack_size_old] <- stack_old
-            names(stack[1:stack_size_old]) <- names(stack_old)
-          }
-          stack_add <- stack_top + n_vis_pa + n_vis_ch
-          stack[(stack_top + 1):(stack_add)] <- c(rep(TRUE, n_vis_pa), rep(FALSE, n_vis_ch))
-          names(stack)[(stack_top + 1):(stack_add)] <- c(visitable_parents, visitable_children)
-          stack_top <- stack_add
-        }
-      } else if (!el) {
+        traverse_up[visitable_parents] <- TRUE
+        traverse_down[visitable_children] <- TRUE
+      } else if (!direction) {
         if (!(el_name %in% z)) {
           visitable_children <- intersect(setdiff(children_unsrt(el_name, G), el_name), an_xyz)
-          n_vis_ch <- length(visitable_children)
-          if (n_vis_ch > 0) {
-            while (n_vis_ch + stack_top > stack_size) {
-              stack_old <- stack
-              stack_size_old <- stack_size
-              stack_size <- stack_size * 2
-              stack <- rep(FALSE, stack_size)
-              stack[1:stack_size_old] <- stack_old
-              names(stack[1:stack_size_old]) <- names(stack_old)
-            }
-            stack_add <- stack_top + n_vis_ch
-            stack[(stack_top + 1):(stack_add)] <- rep(FALSE, n_vis_ch)
-            names(stack)[(stack_top + 1):(stack_add)] <- visitable_children
-            stack_top <- stack_add
-          }
+          traverse_down[visitable_children] <- TRUE
         }
         if (el_name %in% an_z) {
           visitable_parents <- intersect(setdiff(parents_unsrt(el_name, G), el_name), an_xyz)
-          n_vis_pa <- length(visitable_parents)
-          if (n_vis_pa > 0) {
-            while (n_vis_pa + stack_top > stack_size) {
-              stack_old <- stack
-              stack_size_old <- stack_size
-              stack_size <- stack_size * 2
-              stack <- rep(FALSE, stack_size)
-              stack[1:stack_size_old] <- stack_old
-              names(stack[1:stack_size_old] <- stack_old)
-            }
-            stack_add <- stack_top + n_vis_pa
-            stack[(stack_top + 1):(stack_add)] <- rep(TRUE, n_vis_pa)
-            names(stack)[(stack_top + 1):(stack_add)] <- visitable_parents
-            stack_top <- stack_add
-          }
+          traverse_up[visitable_parents] <- TRUE
         }
       }
     }
